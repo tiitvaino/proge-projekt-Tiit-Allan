e8d52939ae5b950705c8fc34fbff4200dc0b8298
A.Loo <loo_allan@yahoo.com>
2019-12-10 13:36:19 +0200

    theme update

--- algneGUI.py
+++ algneGUI.py
@@ -4,6 +4,8 @@ from tkinter import messagebox
 import os
 from PIL import Image, ImageTk
 from andmetöötleja import *
+import tkinter.ttk as ttk 
+from ttkthemes import ThemedStyle
 kõik_andmed = []
 
 def save():
@@ -106,6 +108,8 @@ root_menu = Menu(root)
 root.config(menu = root_menu)
 root.resizable(0, 0)
 
+style = ThemedStyle(root)
+style.set_theme("plastik")
 
 def function(): #Lihtsalt tühi funktsioon nuppudele määramiseks
     pass
@@ -115,34 +119,34 @@ root_menu.add_cascade(label = "Info", menu = file_menu)
 file_menu.add_command(label = "Help", command = alert)
 file_menu.add_command(label = "Info", command = info)
 
-top_frame = Frame(root, borderwidth=1)
+top_frame = ttk.Frame(root, borderwidth=0)
 top_frame.pack(fill=BOTH, expand=False)
-center_frame = Frame(root,relief=RAISED,borderwidth=1)
+center_frame = ttk.Frame(root,relief=RAISED,borderwidth=1, padding = 10)
 center_frame.pack(fill=BOTH, expand=True)
-bottom_frame = Frame(root,relief=RAISED,borderwidth=1)
+bottom_frame = ttk.Frame(root,relief=RAISED,borderwidth=1)
 bottom_frame.pack(fill=BOTH, expand=False)
 
-closeButton = Button(bottom_frame, text="Close", command = quit)
+closeButton = ttk.Button(bottom_frame, text="Close", command = quit)
 closeButton.pack(side=RIGHT, padx=5, pady=5)
 
-okButton = Button(bottom_frame, text="OK", command = lambda:[andmeanalüüs(), save()])
+okButton = ttk.Button(bottom_frame, text="OK", command = lambda:[andmeanalüüs(), save()])
 okButton.pack(side=RIGHT, padx=5, pady=5)
 
-riba = Label(top_frame, text = "Loomade andmete analüüsija", fg = "white", bg = "green")
+riba = ttk.Label(top_frame, text = "Loomade andmete analüüsija", foreground = "white", background = "green", anchor="center")
 riba.pack(fill = "x")
 
-browseButton = Button(master = bottom_frame, text = 'Browse', width = 6, command = browse_file)
+browseButton = ttk.Button(master = bottom_frame, text = 'Browse', width = 6, command = browse_file)
 browseButton.pack(side=LEFT, padx = 2, pady=2)
 
 var = StringVar()
-tabel = Label(bottom_frame, textvariable=var, width=17, bg="white", relief = SUNKEN)
+tabel = ttk.Label(bottom_frame, textvariable=var, width=17, background="white", relief = SUNKEN)
 tabel.pack(side=LEFT, padx = 2, pady = 2)
 
-selectButton = Button(master = bottom_frame, text = 'Select', width = 6, command = select_file)
+selectButton = ttk.Button(master = bottom_frame, text = 'Select', width = 6, command = select_file)
 selectButton.pack(side=LEFT, padx = 2, pady=2)
 
 loc = StringVar()
-tabel2 = Label(bottom_frame, textvariable=loc, width=17, bg="white", relief = SUNKEN)
+tabel2 = ttk.Label(bottom_frame, textvariable=loc, width=17, background="white", relief = SUNKEN)
 tabel2.pack(side=LEFT, padx = 2, pady = 2)
 
 
@@ -153,50 +157,50 @@ center_frame.grid_rowconfigure(1, weight=1)
 center_frame.grid_columnconfigure(2, weight=1)
 center_frame.grid_rowconfigure(2, weight=1)
 
-Label(center_frame, text='Sisestage loomade\narvud kuu alguses', font=("Helvetica 12 bold")).grid(column = 0, columnspan = 2, row=0, padx=5, sticky = S)
+ttk.Label(center_frame, text='Sisestage loomade\narvud kuu alguses', font=("Helvetica 12 bold")).grid(column = 0, columnspan = 2, row=0, padx=5, sticky = S)
 
 validation = center_frame.register(only_numbers)
 validation2 = center_frame.register(numbers_dot)
 
-Label(center_frame, text='uted', font=("Helvetica 10 bold")).grid(column = 1, row=1, padx=5, sticky = S) 
-põhikarja_uted = Entry(center_frame, width = 4, validate="key", validatecommand=(validation, '%S'))
+ttk.Label(center_frame, text='uted', font=("Helvetica 10 bold")).grid(column = 1, row=1, padx=5, sticky = S) 
+põhikarja_uted = ttk.Entry(center_frame, width = 4, validate="key", validatecommand=(validation, '%S'))
 põhikarja_uted.insert(0,0)
 põhikarja_uted.grid(row=2, column = 1, padx=5, sticky = S)
 
-Label(center_frame, text='jäärad', font=("Helvetica 10 bold")).grid(column = 0, row=1, padx=5, sticky = S) 
-põhikarja_jäärad = Entry(center_frame, width = 4, validate="key", validatecommand=(validation, '%S'))
+ttk.Label(center_frame, text='jäärad', font=("Helvetica 10 bold")).grid(column = 0, row=1, padx=5, sticky = S) 
+põhikarja_jäärad = ttk.Entry(center_frame, width = 4, validate="key", validatecommand=(validation, '%S'))
 põhikarja_jäärad.insert(0,0)
 põhikarja_jäärad.grid(row=2, padx=5, sticky = S)
 
-Label(center_frame, text='utikud', font=("Helvetica 10 bold")).grid(column = 1, row=3, padx=5, sticky = S) 
-utikud = Entry(center_frame, width = 4, validate="key", validatecommand=(validation, '%S'))
+ttk.Label(center_frame, text='utikud', font=("Helvetica 10 bold")).grid(column = 1, row=3, padx=5, sticky = S) 
+utikud = ttk.Entry(center_frame, width = 4, validate="key", validatecommand=(validation, '%S'))
 utikud.insert(0,0)
 utikud.grid(row=4, column = 1, padx=5, sticky = S)
 
-Label(center_frame, text='jäärikud', font=("Helvetica 10 bold")).grid(column = 0, row=3, padx=5, sticky = S) 
-jäärikud = Entry(center_frame, width = 4, validate="key", validatecommand=(validation, '%S'))
+ttk.Label(center_frame, text='jäärikud', font=("Helvetica 10 bold")).grid(column = 0, row=3, padx=5, sticky = S) 
+jäärikud = ttk.Entry(center_frame, width = 4, validate="key", validatecommand=(validation, '%S'))
 jäärikud.insert(0,0)
 jäärikud.grid(row=4, padx=5, sticky = S)
 
-Label(center_frame, text='utt-talled', font=("Helvetica 10 bold")).grid(column = 1, row=5, padx=5, sticky = S) 
-utt_talled = Entry(center_frame, width = 4, validate="key", validatecommand=(validation, '%S'))
+ttk.Label(center_frame, text='utt-talled', font=("Helvetica 10 bold")).grid(column = 1, row=5, padx=5, sticky = S) 
+utt_talled = ttk.Entry(center_frame, width = 4, validate="key", validatecommand=(validation, '%S'))
 utt_talled.insert(0,0)
 utt_talled.grid(row=6, column = 1, padx=5, sticky = S)
 
-Label(center_frame, text='jäär-talled', font=("Helvetica 10 bold")).grid(column = 0, row=5, padx=5, sticky = S) 
-jäär_talled = Entry(center_frame, width = 4, validate="key", validatecommand=(validation, '%S'))
+ttk.Label(center_frame, text='jäär-talled', font=("Helvetica 10 bold")).grid(column = 0, row=5, padx=5, sticky = S) 
+jäär_talled = ttk.Entry(center_frame, width = 4, validate="key", validatecommand=(validation, '%S'))
 jäär_talled.insert(0,0)
 jäär_talled.grid(row=6, padx=5, sticky = S)
 
-Label(center_frame, text='Sisestage alguskuupäev', font=("Helvetica 10 bold")).grid(row=7, columnspan = 2, padx=5, sticky = S) 
-algus = Entry(center_frame, validate="key", validatecommand=(validation2, '%S'))
+ttk.Label(center_frame, text='Sisestage alguskuupäev', font=("Helvetica 10 bold")).grid(row=7, columnspan = 2, padx=5, sticky = S) 
+algus = ttk.Entry(center_frame, validate="key", validatecommand=(validation2, '%S'))
 algus.grid(row=8, padx=5, sticky = S, columnspan = 2)
 algus.insert(0, "dd.mm.yyyy")
 algus.bind("<Button-1>", del_ins)
 
 
-Label(center_frame, text='Sisestage lõppkuupäev', font=("Helvetica 10 bold")).grid(row=9, columnspan = 2, padx=5, sticky = S) 
-lõpp = Entry(center_frame, validate="key", validatecommand=(validation2, '%S'))
+ttk.Label(center_frame, text='Sisestage lõppkuupäev', font=("Helvetica 10 bold")).grid(row=9, columnspan = 2, padx=5, sticky = S) 
+lõpp = ttk.Entry(center_frame, validate="key", validatecommand=(validation2, '%S'))
 lõpp.grid(row=10, padx=5, sticky = S, columnspan = 2)
 lõpp.insert(0, "dd.mm.yyyy")
 lõpp.bind("<Button-1>", del_ins2)
@@ -205,45 +209,46 @@ lõpp.bind("<Button-1>", del_ins2)
 lamb = Image.open("lamb.ico")
 resized = lamb.resize((150, 120),Image.ANTIALIAS)
 photo = ImageTk.PhotoImage(resized)
-lambLabel = Label(top_frame, image = photo, bg = "white")
+lambLabel = ttk.Label(top_frame, image = photo, background = "white", anchor="center")
 lambLabel.image = photo
 lambLabel.pack(fill = "x")
 
 
-Label(center_frame, text='Sissetulek', font=("Helvetica 12 bold")).grid(row=0, column=2, padx=5, sticky = S)
-Label(center_frame, text='Kokku').grid(row=1, column=2, padx=5, sticky = S)
-Label(center_frame, text='Teisest rühmast').grid(row=3, column=2, padx=5, sticky = S)
-Label(center_frame, text='Sünd').grid(row=5, column=2, padx=5, sticky = S)
-Label(center_frame, text='Ostetud').grid(row=7, column=2, padx=5, sticky = S) 
-Label(center_frame, text='Muu').grid(row=9, column=2, padx=5, sticky = S)
-e0 = Label(center_frame, relief = SUNKEN, width=17, bg="white")
+ttk.Label(center_frame, text='Sissetulek', font=("Helvetica 12 bold")).grid(row=0, column=2, padx=5, sticky = S)
+ttk.Label(center_frame, text='Kokku').grid(row=1, column=2, padx=5, sticky = S)
+ttk.Label(center_frame, text='Teisest rühmast').grid(row=3, column=2, padx=5, sticky = S)
+ttk.Label(center_frame, text='Sünd').grid(row=5, column=2, padx=5, sticky = S)
+ttk.Label(center_frame, text='Ostetud').grid(row=7, column=2, padx=5, sticky = S) 
+ttk.Label(center_frame, text='Muu').grid(row=9, column=2, padx=5, sticky = S)
+e0 = ttk.Label(center_frame, relief = SUNKEN, width=17, background="white")
 e0.grid(row=2, column=2, padx=5, sticky = S)
-e1 = Label(center_frame, relief = SUNKEN, width=17, bg="white")
+e1 = ttk.Label(center_frame, relief = SUNKEN, width=17, background="white")
 e1.grid(row=4, column=2, padx=5, sticky = S)
-e2 = Label(center_frame, relief = SUNKEN, width=17, bg="white")
+e2 = ttk.Label(center_frame, relief = SUNKEN, width=17, background="white")
 e2.grid(row=6, column=2, padx=5, sticky = S)
-e3 = Label(center_frame, relief = SUNKEN, width=17, bg="white")
+e3 = ttk.Label(center_frame, relief = SUNKEN, width=17, background="white")
 e3.grid(row=8, column=2, padx=5, sticky = S)
-e4 = Label(center_frame, relief = SUNKEN, width=17, bg="white")
+e4 = ttk.Label(center_frame, relief = SUNKEN, width=17, background="white")
 e4.grid(row=10, column=2, padx=5, sticky = S)
 
 
-Label(center_frame, text='Väljaminek', font=("Helvetica 12 bold")).grid(row=0, column=3, padx=5, sticky = S)
-Label(center_frame, text='Kokku').grid(row=1, column=3, padx=5, sticky = S)
-Label(center_frame, text='Teise rühma').grid(row=3, column=3, padx=5, sticky = S)
-Label(center_frame, text='Hukkumine').grid(row=5, column=3, padx=5, sticky = S) 
-Label(center_frame, text='Müük').grid(row=7, column=3, padx=5, sticky = S)
-Label(center_frame, text='Lihaks').grid(row=9, column=3, padx=5, sticky = S)
-e5 = Label(center_frame, relief = SUNKEN, width=17, bg="white")
+ttk.Label(center_frame, text='Väljaminek', font=("Helvetica 12 bold")).grid(row=0, column=3, padx=5, sticky = S)
+ttk.Label(center_frame, text='Kokku').grid(row=1, column=3, padx=5, sticky = S)
+ttk.Label(center_frame, text='Teise rühma').grid(row=3, column=3, padx=5, sticky = S)
+ttk.Label(center_frame, text='Hukkumine').grid(row=5, column=3, padx=5, sticky = S) 
+ttk.Label(center_frame, text='Müük').grid(row=7, column=3, padx=5, sticky = S)
+ttk.Label(center_frame, text='Lihaks').grid(row=9, column=3, padx=5, sticky = S)
+e5 = ttk.Label(center_frame, relief = SUNKEN, width=17, background="white")
 e5.grid(row=2, column=3, padx=5, sticky = S)
-e6 = Label(center_frame, relief = SUNKEN, width=17, bg="white")
+e6 = ttk.Label(center_frame, relief = SUNKEN, width=17, background="white")
 e6.grid(row=4, column=3, padx=5, sticky = S)
-e7 = Label(center_frame, relief = SUNKEN, width=17, bg="white")
+e7 = ttk.Label(center_frame, relief = SUNKEN, width=17, background="white")
 e7.grid(row=6, column=3, padx=5, sticky = S)
-e8 = Label(center_frame, relief = SUNKEN, width=17, bg="white")
+e8 = ttk.Label(center_frame, relief = SUNKEN, width=17, background="white")
 e8.grid(row=8, column=3, padx=5, sticky = S)
-e9 = Label(center_frame, relief = SUNKEN, width=17, bg="white")
+e9 = ttk.Label(center_frame, relief = SUNKEN, width=17, background="white")
 e9.grid(row=10, column=3, padx=5, sticky = S)
+
 
 
 root.mainloop()
